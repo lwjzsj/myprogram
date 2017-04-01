7df6e8c9d49a698675da8e27e0fe5b88524def0e
lwjzsj <1172914545@qq.com>
2017-04-01 15:14:55 +0800

    LoginActivty

--- BroadcastTest/app/src/main/java/com/example/lwjzsj/broadcasttest/LoginActivity.java
+++ BroadcastTest/app/src/main/java/com/example/lwjzsj/broadcasttest/LoginActivity.java
@@ -1,21 +1,41 @@
 package com.example.lwjzsj.broadcasttest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by lwjzsj on 2017/4/1.
  */
 
 public class LoginActivity extends BaseActivity {
-    private EditText account;
-    private EditText password;
-    private Button login;
+    private EditText accountText;
+    private EditText passwordText;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        accountText = (EditText)findViewById(R.id.account);
+        passwordText = (EditText)findViewById(R.id.password);
+        loginButton = (Button)findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = accountText.getText().toString();
+                String password = passwordText.getText().toString();
+                if((account == "admin") &&(password == "12345")){
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(LoginActivity.this,"密码或用户名不正确",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
